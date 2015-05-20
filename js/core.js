92c04c978bed777af373eac37c1aa3cecfe143ec
Linus Yu <ilikethisgames@gmail.com>
2015-05-20 11:55:35 +0800

    修改样式

--- js/core.js
+++ js/core.js
@@ -16,31 +16,31 @@
 	})();
 
 	fxb.loading = function(loading) {
-		fxb.conf.dom.loading.style.display = loading ? 'block': 'none';
+		fxb.conf.dom.loading.style.display = loading ? "block": "none";
 	};
 
     fxb.menu = function(){
         function toggleMenu(){
-            fxb.conf.dom.nav.classList.contains('ex') ?
-            fxb.conf.dom.nav.classList.remove('ex') :
-            fxb.conf.dom.nav.classList.add('ex');        
+            fxb.conf.dom.nav.classList.contains("ex") ?
+            fxb.conf.dom.nav.classList.remove("ex") :
+            fxb.conf.dom.nav.classList.add("ex");
         }
-        fxb.conf.dom.menu.addEventListener('click',function(){
+        fxb.conf.dom.menu.addEventListener("click",function(){
             toggleMenu();
         },false);
-        fxb.conf.dom.nav.addEventListener('click',function(){
+        fxb.conf.dom.nav.addEventListener("click",function(){
             toggleMenu();            
         },false);     
     }
     
 	fxb.ajax = function(details, onload) {
 
-		if (typeof details === 'string') details = {
-			'url': details
+		if (typeof details === "string") details = {
+			"url": details
 		};
 		if (onload) details.onload = onload;
-		details.method = details.method || 'GET';
-		details.body = details.body || '';
+		details.method = details.method || "GET";
+		details.body = details.body || "";
 
 		var xhr = new XMLHttpRequest();
 		xhr.onreadystatechange = function() {
@@ -49,7 +49,7 @@
 				if (xhr.status === 200) {
 					if (details.onload) details.onload.call(xhr);
 				} else {
-					console.error('Network fail: [%s] %s', details.method, details.url);
+					console.error("Network fail: [%s] %s", details.method, details.url);
 					if (details.onerror) details.onerror.call(xhr);
 				}
 			}
@@ -60,12 +60,12 @@
 	}
 
 	fxb.link = function() {
-		var n = fxb.conf.dom.nav.querySelectorAll('a');
-		var events = ['mousedown', 'focus', 'touchstart'];
+		var n = fxb.conf.dom.nav.querySelectorAll("a");
+		var events = ["mousedown", "focus", "touchstart"];
 		for (var i = 0,
 		l = n.length; i < l; i++)(function(a) {
 			var updateLink = function() {
-				a.href = a.href.replace(/^.*\/([^\/]*)\.html/, '#$1');               
+				a.href = a.href.replace(/^.*\/([^\/]*)\.html/, "#$1");               
 				events.forEach(function(e) {
 					a.removeEventListener(e, updateLink);
 				});
@@ -77,23 +77,23 @@
 	};
 
 	fxb.nav = function() {
-		var page = location.hash.replace(/^(#?)([^?#]*)((\?|#).*)?$/, '$2') || 'home';
-		var cu = document.querySelector('.cu');
-		if (cu) cu.classList.remove('cu');
-		document.getElementById(page).classList.add('cu');
+		var page = location.hash.replace(/^(#?)([^?#]*)((\?|#).*)?$/, "$2") || "home";
+		var cu = document.querySelector(".cu");
+		if (cu) cu.classList.remove("cu");
+		document.getElementById(page).classList.add("cu");
 		return page;
 	};
 
 	fxb.load = function() {
 		var page = fxb.nav();
-		fxb.conf.dom.main.innerHTML = '';
+		fxb.conf.dom.main.innerHTML = "";
 		fxb.loading(true);
 		fxb.ajax({
-			url: fxb.conf.template + '/' + page + '.html',
+			url: fxb.conf.template + "/" + page + ".html",
 			onload: function() {
 				fxb.loading(false);
 				var addScript = function(script) {
-					var s = document.createElement('script');
+					var s = document.createElement("script");
 					s.innerHTML = script.innerHTML;
 					document.head.appendChild(s);
 					setTimeout(function() {
@@ -101,12 +101,12 @@
 					},
 					0);
 				};
-				var dom = new DOMParser().parseFromString(this.responseText, 'text/html');
-				fxb.conf.dom.main.innerHTML = dom.querySelector('body').innerHTML;
-				var scripts = dom.querySelectorAll('head script.exec');
+				var dom = new DOMParser().parseFromString(this.responseText, "text/html");
+				fxb.conf.dom.main.innerHTML = dom.querySelector("body").innerHTML;
+				var scripts = dom.querySelectorAll("head script.exec");
 				for (var i = 0,
 				l = scripts.length; i < l; i++) addScript(scripts[i]);
-				document.title = dom.querySelector('title').textContent;
+				document.title = dom.querySelector("title").textContent;
 			},
 			onerror: function() {
 				fxb.loading(false);
